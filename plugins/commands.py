5043c1abfdb735dae0581de11a47b039155870f3
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2024-03-14 21:47:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -218,9 +218,9 @@ async def start(client, message):
     k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this ALL Files/Videos to your Saved Messages and Start Download there</i></b>")
     await asyncio.sleep(600)
     for x in filesarr:
-    await x.delete()
-    await k.edit_text("<b>Your All Files/Videos is successfully deleted!!!</b>")
-    return 
+        await x.delete()
+        await k.edit_text("<b>Your All Files/Videos is successfully deleted!!!</b>")
+        return 
 
 @Client.on_message(filters.command('index_channels') & filters.user(ADMINS))
 async def channels_info(bot, message):
